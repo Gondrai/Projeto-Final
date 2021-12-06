406d4b01e84c6ee70698429bcddc201534805785
Isabela de Gondra <66333927+Gondrai@users.noreply.github.com>
2021-12-06 19:34:56 -0300

    Update DateInOut.h

--- DateInOut.h
+++ DateInOut.h
@@ -1,3 +1,18 @@
+#include <string>
+#include <vector>
+// Estrutura para salvar data de pacientes
+struct Date
+{
+    int day;
+    int mon;
+    int year;
+    int hour;
+    int min;
+    int sec;
+    bool valid; //false -> acabou de entrar: só printa data de entrada
+                //true -> entrou e saiu: printa entrada e saida
+};
+
 class DateInOut
 {
 public:
