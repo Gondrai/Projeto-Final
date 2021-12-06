4e983594719215b0c216be15a011210bc9d5c16a
RenatoPorfirio <95599633+RenatoPorfirio@users.noreply.github.com>
2021-12-06 00:27:25 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -28,40 +28,6 @@ public:
     struct Date in;  // Data de entrada
     struct Date out; // Data de saída
 };
-class Leito
-{
-private:
-    int ocupado;
-
-public:
-    Leito();
-    Leito(int valor);
-    ~Leito();
-    void set_ocupacao(int valor); //Definir como ocupado ou desocupado
-    int get_ocupacao();
-};
-
-Leito::Leito() {}
-
-//Armazena o valor 0 = livre/ 1 = ocupado
-Leito::Leito(int valor) {
-    this->ocupado = valor;
-}
-
-Leito::~Leito() {}
-
-//Define a situação do leito
-void Leito::set_ocupacao(int valor)
-{
-    //0 = livre / 1 = ocupado
-    this->ocupado = valor;
-}
-
-//Retorna a situação do leito
-int Leito::get_ocupacao()
-{
-    return this->ocupado;
-}
 
 class Sala
 {
