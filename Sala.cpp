8b37836df1c0e19084400e9c422e991bbdb039fd
Isabela de Gondra <66333927+Gondrai@users.noreply.github.com>
2021-12-06 19:33:49 -0300

    Create Sala.cpp

---
+++ Sala.cpp
@@ -0,0 +1,83 @@
+class Sala
+{
+private:
+    vector<Leito> leitos;
+
+public:
+    Sala();
+    Sala(int total_leitos);
+    ~Sala();
+    void Create();
+    void Create(Leito leito);
+    void Delete();
+    void Delete(int pos);
+    int get_numero_leitos();
+    int get_numero_leitos_ocupados();
+    int get_numero_leitos_desocupados();
+};
+
+Sala::Sala() {}
+
+//Cria uma sala com o total de leitos desejável
+Sala::Sala(int total_leitos)
+{
+    for (int i = 0; i < total_leitos; i++)
+    {
+        Leito leito;
+        leito.set_ocupacao(0);
+        this->leitos.push_back(leito);
+    }
+}
+
+//Adiciona um leito  na sala
+void Sala::Create()
+{
+    Leito leito;
+    this->leitos.push_back(leito);
+}
+
+//Adiciona um leito na sala passando o leito desejado
+void Sala::Create(Leito leito)
+{
+    this->leitos.push_back(leito);
+}
+
+//Deleta um leito da sala
+void Sala::Delete()
+{
+    this->leitos.pop_back();
+}
+
+//Deleta um leito informando a posição no vetor
+void Sala::Delete(int pos)
+{
+    this->leitos.erase(this->leitos.begin() + pos);
+}
+
+Sala::~Sala() {}
+
+//Retorna o número de leitos
+int Sala::get_numero_leitos()
+{
+    return this->leitos.size();
+}
+
+//Retorna o número de leitos ocupados(ocupado = 1)
+int Sala::get_numero_leitos_ocupados()
+{
+    int total = 0;
+    for (int i = 0; i < this->leitos.size(); i++)
+        if (this->leitos[i].get_ocupacao() == 1)
+            total += 1;
+    return total;
+}
+
+//Retorna o número de leitos desocupados(ocupado = 0)
+int Sala::get_numero_leitos_desocupados()
+{
+    int total = 0;
+    for (int i = 0; i < this->leitos.size(); i++)
+        if (this->leitos[i].get_ocupacao() == 0)
+            total += 1;
+    return total;
+}
