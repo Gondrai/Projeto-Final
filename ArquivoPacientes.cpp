6d64361d32cc94887f636a86d256067693373a5d
RenatoPorfirio <95599633+RenatoPorfirio@users.noreply.github.com>
2021-12-06 01:50:12 -0300

    Update ArquivoPacientes.cpp

--- ArquivoPacientes.cpp
+++ ArquivoPacientes.cpp
@@ -7,19 +7,6 @@ using namespace std;
 //Construtor
 ArquivoPacientes::ArquivoPacientes()
 {
-    struct tm *data_tempo;
-    time_t segundos;
-    time(&segundos);
-
-    data_tempo = localtime(&segundos);
-
-    //inclusao de data e hora ao criar o arquivo
-    data_in_out[0] = data_tempo->tm_mday;
-    data_in_out[1] = data_tempo->tm_mon + 1;
-    data_in_out[2] = data_tempo->tm_year + 1900;
-    data_in_out[3] = data_tempo->tm_hour;
-    data_in_out[4] = data_tempo->tm_min;
-    data_in_out[5] = data_tempo->tm_sec;
 }
 
 //Método para criar pacientes
