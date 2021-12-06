3e033474ddea1514434315ca78bcc9a739d7e24f
Isabela de Gondra <66333927+Gondrai@users.noreply.github.com>
2021-12-06 00:30:58 -0300

    Create ArquivoPacientes.h

---
+++ ArquivoPacientes.h
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+class ArquivoPacientes
+{
+private:
+    vector<Paciente> pacientes; //vetor para armazenar os pacientes
+    vector<DateInOut> dateInOut; //vetor para armazenar as datas de saida e entrada de cada um
+    int data_in_out[11]; // data de entrada e saída
+
+public:
+    ArquivoPacientes();
+    void Create(Paciente P);          // Cria um paciente
+    void Update(Paciente p, int pos); // Atualiza um paciente
+    void Delete();                    // Deleta o último paciente inserido
+    void DeleteWithCpf(string cpf);   // Deleta um paciente em especifico
+    void MarcarSaida(string cpf);     // Marca quando um paciente saiu
+    Paciente Read(int i);             // Obtem um paciente por posição
+    Paciente Read(string cpf);        // Obtem um paciente pelo cpf
+    int ReadPos(string cpf);          // Obtem a posição no vetor para um paciente
+    int Total();                      // Retorna total de pacientes
+    int TotalAlta();                  // Obtem total de altas
+    int TotalInternados();            // Total de internados
+    DateInOut getDataPaciente(int pos); // Retorna as datas de um paciente
+};
