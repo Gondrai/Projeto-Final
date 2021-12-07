a948b9000b9705f874384bd286d6c10656841452
Isabela de Gondra <66333927+Gondrai@users.noreply.github.com>
2021-12-06 21:57:50 -0300

    Create Ala.h

---
+++ Ala.h
@@ -0,0 +1,22 @@
+#include <string>
+
+class Ala
+{
+private:
+    vector<Sala> salas; //Vetor para armazenar as salas da Ala
+
+public:
+    Ala();
+    Ala(int total_salas);
+    ~Ala();
+    void Create();                   // Cria uma sala nova
+    void Create(Sala sala);          // Insere uma sala
+    Sala Read(int pos);              // Obtem uma sala em especifico
+    void Update(Sala sala, int pos); // Atualiza uma posição
+    void Delete();                   // Deleta a última sala
+    void Delete(int pos);            // Deleta uma posição
+    float TaxaOcupacaoLeito();       // Taxa de ocupação
+    float TaxaDesocupacaoLeito();    // Taxa de desoucupeção
+    int TotalLeitos();               // Total de leitos
+    int TotalSalas();                // Total salas
+};
