21aa8ea7c0c79a1a657f1e380d7d110a0f3d016f
Isabela de Gondra <66333927+Gondrai@users.noreply.github.com>
2021-12-06 21:48:47 -0300

    Update Sala.cpp

--- Sala.cpp
+++ Sala.cpp
@@ -2,23 +2,6 @@
 #include <vector>
 #include <iostream>
 #include "Sala.h"
-class Sala
-{
-private:
-    vector<Leito> leitos;
-
-public:
-    Sala();
-    Sala(int total_leitos);
-    ~Sala();
-    void Create();
-    void Create(Leito leito);
-    void Delete();
-    void Delete(int pos);
-    int get_numero_leitos();
-    int get_numero_leitos_ocupados();
-    int get_numero_leitos_desocupados();
-};
 
 Sala::Sala() {}
 
