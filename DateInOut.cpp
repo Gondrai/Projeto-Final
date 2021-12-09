0945bc8660978357ff3810c03a72b14da28d23f5
RenatoPorfirio <95599633+RenatoPorfirio@users.noreply.github.com>
2021-12-09 20:20:28 -0300

    Update DateInOut.cpp

--- DateInOut.cpp
+++ DateInOut.cpp
@@ -15,14 +15,12 @@ void DateInOut::gen_Date_in(){
     time(&segundos);
     data_tempo = localtime(&segundos);
 
-    //Estrutura para armazenar a data na variÃ¡vel date
     this->in.day = data_tempo->tm_mday;
     this->in.mon = data_tempo->tm_mon + 1;
     this->in.year = data_tempo->tm_year + 1900;
     this->in.hour = data_tempo->tm_hour;
     this->in.min = data_tempo->tm_min;
     this->in.sec = data_tempo->tm_sec;
-    // Salva quando o usuÃ¡rio entrou
 }
 
 void DateInOut::gen_Date_out(){
@@ -30,15 +28,13 @@ void DateInOut::gen_Date_out(){
     time_t segundos;
     time(&segundos);
     data_tempo = localtime(&segundos);
-
-    //Estrutura para armazenar a data na variÃ¡vel date
-	this->out.day = data_tempo->tm_mday;
+	
+    this->out.day = data_tempo->tm_mday;
     this->out.mon = data_tempo->tm_mon + 1;
     this->out.year = data_tempo->tm_year + 1900;
     this->out.hour = data_tempo->tm_hour;
     this->out.min = data_tempo->tm_min;
     this->out.sec = data_tempo->tm_sec;
-    // Salva quando o usuÃ¡rio saiu
 }
 
 struct Date DateInOut::get_Date_in(){
