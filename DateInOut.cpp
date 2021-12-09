8cb59582e61973c6ed64cc5879392781c9eac22a
RenatoPorfirio <95599633+RenatoPorfirio@users.noreply.github.com>
2021-12-08 23:17:39 -0300

    Update DateInOut.cpp

--- DateInOut.cpp
+++ DateInOut.cpp
@@ -1,10 +1,19 @@
 #include "DateInOut.h"
 
-class DateInOut
-{
-public:
-    struct Date in;  // Data de entrada
-    struct Date out; // Data de saída
-};
+void DateInOut::gen_Date_in(){
+    struct tm *data_tempo;
+    time_t segundos;
+    time(&segundos);
+    data_tempo = localtime(&segundos);
 
-// Salva quando o usuário entrou e quanto saiu
+    //Estrutura para armazenar a data na variável date
+    struct Date *date = new struct Date();
+    date->day = data_tempo->tm_mday;
+    date->mon = data_tempo->tm_mon + 1;
+    date->year = data_tempo->tm_year + 1900;
+    date->hour = data_tempo->tm_hour;
+    date->min = data_tempo->tm_min;
+    date->sec = data_tempo->tm_sec;
+    in = date;
+}
+// Salva quando o usuário entrou
