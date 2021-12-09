6b494e2e3980383696275181ed4e65a5b8dc3d1d
RenatoPorfirio <95599633+RenatoPorfirio@users.noreply.github.com>
2021-12-09 01:35:37 -0300

    Update DateInOut.h

--- DateInOut.h
+++ DateInOut.h
@@ -17,14 +17,14 @@ struct Date
 class DateInOut
 {
 public:
-    struct Date *in;  // Data de entrada
-    struct Date *out; // Data de saída
-    DateInOut(){this->in = this->out = NULL;}
-    ~DateInOut(){delete this->in; delete this->out;}
-    void gen_Date_in(); // Adiciona a data atual na variável da data de entrada
-    void gen_Date_out(); // Adiciona a data atual na variável da data de saída
-    struct Date *get_Date_in(); // Retorna a data de entrada
-    struct Date *get_Date_out(); // Retorna a data de saída
+    struct Date in;  // Data de entrada
+    struct Date out; // Data de saida
+    DateInOut();
+    ~DateInOut(){}
+    void gen_Date_in(); // Adiciona a data atual na variÃ¡vel da data de entrada
+    void gen_Date_out(); // Adiciona a data atual na variÃ¡vel da data de saÃ­da
+    struct Date get_Date_in(); // Retorna a data de entrada
+    struct Date get_Date_out(); // Retorna a data de saÃ­da
 };
 
 #endif
