ae84f06c001fd86b96a5819fa88640720306bee7
RenatoPorfirio <95599633+RenatoPorfirio@users.noreply.github.com>
2021-12-09 20:13:21 -0300

    Update Sala.cpp

--- Sala.cpp
+++ Sala.cpp
@@ -1,8 +1,8 @@
 #include "Sala.h"
 
 Sala::Sala() {}
+Sala::~Sala() {}
 
-//Cria uma sala com o total de leitos desejÃ¡vel
 Sala::Sala(int total_leitos)
 {
     for (int i = 0; i < total_leitos; i++)
@@ -13,20 +13,17 @@ Sala::Sala(int total_leitos)
     }
 }
 
-//Adiciona um leito  na sala
 void Sala::Create()
 {
     Leito leito;
     this->leitos.push_back(leito);
 }
 
-//Deleta o ultimo leito da sala
 void Sala::Delete()
 {
     this->leitos.pop_back();
 }
 
-//Deleta um leito informando a posiÃ§Ã£o no vetor
 void Sala::Delete(int pos)
 {
     this->leitos.erase(this->leitos.begin() + pos);
@@ -37,15 +34,11 @@ void Sala::set_ocupacao_leito(int pos, int ocp)
     this->leitos[pos].set_ocupacao(ocp);
 }
 
-Sala::~Sala() {}
-
-//Retorna o nÃºmero de leitos
 int Sala::get_numero_leitos()
 {
     return this->leitos.size();
 }
 
-//Retorna o nÃºmero de leitos ocupados(ocupado = 1)
 int Sala::get_numero_leitos_ocupados()
 {
     int total = 0;
@@ -55,7 +48,6 @@ int Sala::get_numero_leitos_ocupados()
     return total;
 }
 
-//Retorna o nÃºmero de leitos desocupados(ocupado = 0)
 int Sala::get_numero_leitos_desocupados()
 {
     int total = 0;
@@ -63,11 +55,4 @@ int Sala::get_numero_leitos_desocupados()
         if (!this->leitos[i].get_ocupacao())
             total += 1;
     return total;
-}
-
-int main(){
-	Sala s(10);
-	s.set_ocupacao_leito(3, 1);
-	cout << s.get_numero_leitos_desocupados() << endl;
-	return 0;
 }
