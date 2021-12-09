dfb9cb06c3ed5477d56e37bf5a0fbeb2cab00591
RenatoPorfirio <95599633+RenatoPorfirio@users.noreply.github.com>
2021-12-09 01:36:19 -0300

    Update DateInOut.cpp

--- DateInOut.cpp
+++ DateInOut.cpp
@@ -1,21 +1,28 @@
 #include "DateInOut.h"
 
+DateInOut::DateInOut(){
+	this->in.day = -1;
+	this->in.mon = -1;
+	this->in.year = -1;
+	this->in.hour = -1;
+	this->in.min = -1;
+	this->in.sec = -1;
+}
+
 void DateInOut::gen_Date_in(){
     struct tm *data_tempo;
     time_t segundos;
     time(&segundos);
     data_tempo = localtime(&segundos);
 
-    //Estrutura para armazenar a data na variável date
-    struct Date *date = new struct Date();
-    date->day = data_tempo->tm_mday;
-    date->mon = data_tempo->tm_mon + 1;
-    date->year = data_tempo->tm_year + 1900;
-    date->hour = data_tempo->tm_hour;
-    date->min = data_tempo->tm_min;
-    date->sec = data_tempo->tm_sec;
-    this->in = date;
-    // Salva quando o usuário entrou
+    //Estrutura para armazenar a data na variÃ¡vel date
+    this->in.day = data_tempo->tm_mday;
+    this->in.mon = data_tempo->tm_mon + 1;
+    this->in.year = data_tempo->tm_year + 1900;
+    this->in.hour = data_tempo->tm_hour;
+    this->in.min = data_tempo->tm_min;
+    this->in.sec = data_tempo->tm_sec;
+    // Salva quando o usuÃ¡rio entrou
 }
 
 void DateInOut::gen_Date_out(){
@@ -24,22 +31,20 @@ void DateInOut::gen_Date_out(){
     time(&segundos);
     data_tempo = localtime(&segundos);
 
-    //Estrutura para armazenar a data na variável date
-    struct Date *date = new struct Date();
-    date->day = data_tempo->tm_mday;
-    date->mon = data_tempo->tm_mon + 1;
-    date->year = data_tempo->tm_year + 1900;
-    date->hour = data_tempo->tm_hour;
-    date->min = data_tempo->tm_min;
-    date->sec = data_tempo->tm_sec;
-    this->out = date;
-    // Salva quando o usuário saiu
+    //Estrutura para armazenar a data na variÃ¡vel date
+	this->out.day = data_tempo->tm_mday;
+    this->out.mon = data_tempo->tm_mon + 1;
+    this->out.year = data_tempo->tm_year + 1900;
+    this->out.hour = data_tempo->tm_hour;
+    this->out.min = data_tempo->tm_min;
+    this->out.sec = data_tempo->tm_sec;
+    // Salva quando o usuÃ¡rio saiu
 }
 
-struct Date *DateInOut::get_Date_in(){
+struct Date DateInOut::get_Date_in(){
     return this->in;
 }
 
-struct Date *DateInOut::get_Date_out(){
+struct Date DateInOut::get_Date_out(){
     return this->out;
 }
