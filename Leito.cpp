7199943f15ce1bee62effd536ae9e33701f6a234
RenatoPorfirio <95599633+RenatoPorfirio@users.noreply.github.com>
2021-12-09 02:05:56 -0300

    Update Leito.cpp

--- Leito.cpp
+++ Leito.cpp
@@ -3,21 +3,21 @@
 Leito::Leito() {}
 
 //Armazena o valor 0 = livre/ 1 = ocupado
-Leito::Leito(int valor) {
+Leito::Leito(bool valor) {
     this->ocupado = valor;
 }
 
 Leito::~Leito() {}
 
 //Define a situação do leito
-void Leito::set_ocupacao(int valor)
+void Leito::set_ocupacao(bool valor)
 {
     //0 = livre / 1 = ocupado
     this->ocupado = valor;
 }
 
 //Retorna a situação do leito
-int Leito::get_ocupacao()
+bool Leito::get_ocupacao()
 {
     return this->ocupado;
 }
