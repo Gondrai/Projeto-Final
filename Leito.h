775c3d12ccb2ce403f39ec2c16f3ea47734f5470
RenatoPorfirio <95599633+RenatoPorfirio@users.noreply.github.com>
2021-12-09 02:04:07 -0300

    Update Leito.h

--- Leito.h
+++ Leito.h
@@ -1,12 +1,19 @@
+#ifndef LEITO_H
+#define LEITO_H
+
+using std::bool;
+
 class Leito
 {
 private:
-    int ocupado;
+    bool ocupado;
 
 public:
     Leito();
-    Leito(int valor);
+    Leito(bool valor);
     ~Leito();
-    void set_ocupacao(int valor); //Definir como ocupado ou desocupado
+    void set_ocupacao(bool valor); //Definir como ocupado ou desocupado
     int get_ocupacao();
 };
+
+#endif
