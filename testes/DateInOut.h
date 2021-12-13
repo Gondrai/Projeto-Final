42270ac758cc1799e949fb9e9b05979a4d4e5014
RenatoPorfirio <95599633+RenatoPorfirio@users.noreply.github.com>
2021-12-13 00:27:29 -0300

    Update DateInOut.h

--- testes/DateInOut.h
+++ testes/DateInOut.h
@@ -2,7 +2,6 @@
 #define DATEINOUT_H
 
 #include <time.h>
-#include <cstdlib>
 // Estrutura para salvar data de pacientes
 struct Date
 {
@@ -21,10 +20,10 @@ public:
     struct Date out; // Data de saida
     DateInOut(); // Inicializa os atributos de in e out com valor -1
     ~DateInOut(){}
-    void gen_Date_in(); // Adiciona a data atual na variável da data de entrada
-    void gen_Date_out(); // Adiciona a data atual na variável da data de saída
+    void gen_Date_in(); // Adiciona a data atual na variÃ¡vel da data de entrada
+    void gen_Date_out(); // Adiciona a data atual na variÃ¡vel da data de saÃ­da
     struct Date get_Date_in(); // Retorna a data de entrada
-    struct Date get_Date_out(); // Retorna a data de saída
+    struct Date get_Date_out(); // Retorna a data de saÃ­da
 };
 
 #endif
